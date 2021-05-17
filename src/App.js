ff532205becfb6c3e32a63e638bd4c979b44dc09
Tharindu7Kanchana <48875349+Tharindu7Kanchana@users.noreply.github.com>
2021-05-18 00:17:59 +0530

    connect backend to front

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
+      <div className = "container">
       <Navbar/>
         <br/>
         <Route path="/" exact component={ExercisesList} />
         <Route path="/edit/:id" component={EditExercises} />
         <Route path="/create" component={CreateExercises} />
         <Route path="/user" component={CreateUser}/>
+        </div>
     </Router>
   );
 }
